4917cc23db69a7c0b59573998a52811276e1ddb9
ralvescosta <rafael.rac.mg@gmail.com>
2025-04-19 09:07:20 -0300

    feat: toggle copilot

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,5 +1,20 @@
 return {
   {
     'github/copilot.vim',
+    config = function()
+      vim.g.copilot_enabled = false
+
+      vim.api.nvim_create_user_command('CopilotToggle', function()
+        if vim.g.copilot_enabled == 1 then
+          vim.g.copilot_enabled = 0
+          print 'Copilot: OFF'
+        else
+          vim.g.copilot_enabled = 1
+          print 'Copilot: ON'
+        end
+      end, {})
+
+      vim.keymap.set('n', '<leader>tc', ':CopilotToggle<CR>', { desc = '[T]oggle [C]opilot', silent = true })
+    end,
   },
 }
