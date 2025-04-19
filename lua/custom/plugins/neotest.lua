644338b013f13b197b8fd356538e4fde074bebd9
ralvescosta <rafael.rac.mg@gmail.com>
2025-04-19 09:07:35 -0300

    feat: neotest config

--- lua/custom/plugins/neotest.lua
+++ lua/custom/plugins/neotest.lua
@@ -24,10 +24,10 @@ return {
         },
       }
 
-      vim.keymap.set('n', '<leader>ts', "<cmd>lua require('neotest').summary.toggle()<CR>", { desc = 'Toggle [N]eotest Summary' })
-      vim.keymap.set('n', '<leader>tr', "<cmd>lua require('neotest').run.run()<CR>", { desc = 'Run [N]earest test to the cursor' })
-      vim.keymap.set('n', '<leader>tf', "<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<CR>", { desc = 'Run entire file' })
-      vim.keymap.set('n', '<leader>td', "<cmd>lua require('neotest').run.run({ strategy = 'dap' })<CR>", { desc = 'Debug [N]earest test' })
+      vim.keymap.set('n', '<leader>ts', "<cmd>lua require('neotest').summary.toggle()<CR>", { desc = '[T]oggle [N]eotest Summary' })
+      vim.keymap.set('n', '<leader>rt', "<cmd>lua require('neotest').run.run()<CR>", { desc = '[R]un [N]earest test to the cursor' })
+      vim.keymap.set('n', '<leader>rf', "<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<CR>", { desc = '[R]un entire file' })
+      vim.keymap.set('n', '<leader>dt', "<cmd>lua require('neotest').run.run({ strategy = 'dap' })<CR>", { desc = 'Debug [N]earest test' })
       vim.keymap.set('n', ']t', "<cmd>lua require('neotest').jump.next({ status = 'failed' })<CR>", { desc = 'Jump to the next failed test' })
       vim.keymap.set('n', '[t', "require('neotest').jump.prev({ status = 'failed' })<CR>", { desc = 'Jump to the previous failed test' })
     end,
