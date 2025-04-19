ba481ccb9a3ce8ca6ff386ed2a1e5620f3fddd33
ralvescosta <rafael.rac.mg@gmail.com>
2025-04-19 08:44:01 -0300

    feat: lazygit

--- lua/custom/plugins/lazygit.lua
+++ lua/custom/plugins/lazygit.lua
@@ -6,6 +6,16 @@ return {
     },
     config = function()
       vim.keymap.set('n', '<leader>tg', ':LazyGit<CR>', { silent = true, noremap = true, desc = '[T]oggle [L]azy Git' })
+
+      vim.api.nvim_create_autocmd('TermClose', {
+        callback = function(args)
+          local bufnr = args.buf
+          local name = vim.api.nvim_buf_get_name(bufnr)
+          if name:match 'lazygit' then
+            vim.cmd 'Neotree git_status refresh'
+          end
+        end,
+      })
     end,
   },
 }
